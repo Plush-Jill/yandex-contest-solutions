97a00f6626dba064dbafe8ad79527854270e30ba
Plush-Jill <bockarevvlad23@gmail.com>
2024-11-28 14:22:37 +0700

    Написал первую задачу, тесты из примеров проходит, лишний вывод закомменчен.

---
+++ task-1/main.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+#include <vector>
+#include <format>
+
+
+class Crossword {
+private:
+    int m_row_count;
+    int m_column_count;
+    std::vector<std::vector<char>> m_grid;
+    std::string m_min_word {"zzzzzzzzz"};
+    int m_counter {};
+    enum class SearchDirection {
+        Right,
+        Down
+    };
+
+    bool can_start_search_from_this_point(int row, int column, SearchDirection direction) {
+        bool is_zero_row = row == 0;
+        bool is_zero_column = column == 0;
+        bool is_white_cell = m_grid[row][column] != '#';
+
+        if (is_zero_row && is_zero_column && is_white_cell) {
+            return true;
+        }
+        if (is_zero_row && direction == SearchDirection::Down && is_white_cell) {
+            return true;
+        }
+        if (is_zero_column && direction == SearchDirection::Right && is_white_cell) {
+            return true;
+        }
+        if (!is_zero_row && direction == SearchDirection::Down) {
+            if (m_grid[row - 1][column] == '#') {
+                return true;
+            }
+        }
+        if (!is_zero_column && direction == SearchDirection::Right) {
+            if (m_grid[row][column - 1] == '#') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void dfs(int row, int column, SearchDirection direction, std::string accumulated_word, bool is_first) {
+        if (row >= m_row_count || column >= m_column_count) {
+            return;
+        }
+
+        char current_symbol = m_grid[row][column];
+//        std::cout << std::format("{}. pos: ({}, {}), char: {}, dir: {}",
+//                                 m_counter++,
+//                                 row,
+//                                 column,
+//                                 current_symbol,
+//                                 direction == SearchDirection::Down ? "down" : "right") << std::endl;
+
+        if (current_symbol == '#') {
+//            std::cout << "got \'#\'" << std::endl;
+//            std::cout << std::format("{} < {} ?", accumulated_word, m_min_word) << std::endl;
+            if (accumulated_word < m_min_word && accumulated_word.size() >= 2) {
+//                std::cout << std::format("{}. \"{}\" < \"{}\", \"{}\" now is new min word", m_counter, accumulated_word,
+//                                         m_min_word, accumulated_word) << std::endl;
+                m_min_word = accumulated_word;
+                return;
+            }
+        } else if (row == m_row_count - 1 && direction == SearchDirection::Down) {
+            accumulated_word += current_symbol;
+//            std::cout << std::format("{} < {} ?", accumulated_word, m_min_word) << std::endl;
+            if (accumulated_word < m_min_word && accumulated_word.size() >= 2) {
+//                std::cout << std::format("{}. \"{}\" < \"{}\", \"{}\" now is new min word", m_counter, accumulated_word,
+//                                         m_min_word, accumulated_word) << std::endl;
+                m_min_word = accumulated_word;
+                return;
+            }
+        } else if (column == m_column_count - 1 && direction == SearchDirection::Right) {
+            accumulated_word += current_symbol;
+//            std::cout << std::format("{} < {} ?", accumulated_word, m_min_word) << std::endl;
+            if (accumulated_word < m_min_word && accumulated_word.size() >= 2) {
+//                std::cout << std::format("{}. \"{}\" < \"{}\", \"{}\" now is new min word", m_counter, accumulated_word,
+//                                         m_min_word, accumulated_word) << std::endl;
+                m_min_word = accumulated_word;
+                return;
+            }
+        } else {
+            accumulated_word += current_symbol;
+
+            if (direction == SearchDirection::Right) {
+                dfs(row, column + 1, direction, accumulated_word, false);
+            } else if (direction == SearchDirection::Down){
+                dfs(row + 1, column, direction, accumulated_word, false);
+            }
+        }
+
+
+    }
+
+public:
+
+    void print_crossword() {
+        for (int i{}; i < m_row_count; ++i) {
+            for (int j{}; j < m_column_count; ++j) {
+                char c = m_grid[i][j];
+                std::cout << c;
+            }
+            std::cout << "\n";
+        }
+    }
+
+    Crossword(int row_count, int column_count){
+        m_row_count = row_count;
+        m_column_count = column_count;
+        m_grid = std::vector<std::vector<char>>(m_row_count, std::vector<char>(m_column_count));
+
+
+
+        for (int i {}; i < m_row_count; ++i) {
+            for (int j {}; j < m_column_count; ++j) {
+                std::cin >> m_grid[i][j];
+            }
+        }
+    }
+
+    std::string find_min_word() {
+        for (int i {}; i < m_row_count; ++i) {
+            for (int j{}; j < m_column_count; ++j) {
+
+                if (can_start_search_from_this_point(i, j, SearchDirection::Right)) {
+                    dfs(i, j, SearchDirection::Right, "", true);
+                }
+                if (can_start_search_from_this_point(i, j, SearchDirection::Down)) {
+                    dfs(i, j, SearchDirection::Down, "", true);
+                }
+
+            }
+        }
+
+        return m_min_word;
+    }
+};
+
+
+int main() {
+    int row_count;
+    int column_count;
+
+    std::cin >> row_count >> column_count;
+
+    Crossword crossword {row_count, column_count};
+//    crossword.print_crossword();
+//    std::cout << "\n\n";
+    std::cout << crossword.find_min_word();
+    return 0;
+}
